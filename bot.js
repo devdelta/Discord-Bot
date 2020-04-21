9d53b1d094a797d1545f86cfed45fcfa46cca74f
Christoffer <CrilleMega@gmail.com>
2020-04-21 22:56:19 +0200

    Upgraded to Discord.js from Discord.io (unsupported)

--- bot.js
+++ bot.js
@@ -1,46 +1,24 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, 
-{
-    colorize: true
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const Token = require("./auth.json").token;
+
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+  bot.user.setActivity("Headbanging to hardstyle."); 
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+bot.on('message', msg => {
+    if(msg.author.bot){return;}
+    var m = msg.content.toLowerCase();
+
+  if (m === 'plz hardstyle') 
+  {
+    msg.reply("");
+  }
+  if (m.includes("crille", 0))
+  {
+    msg.reply("Crille is hot. ;)");
+  }
 });
-bot.on('ready', function (evt) 
-{
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'crille':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'OWO'
-                });
-            case 'nou':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'No u.'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+bot.login(Token);
